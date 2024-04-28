70ec2123ef2d699f9d3f101f073ea9cd72268548
shaojha <138504804+shasojha@users.noreply.github.com>
2024-04-29 01:33:19 +0530

    normal changes

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:4000/api/";
-export const base_img = "http://localhost:4000/uploads/";
+export const base_url = "http://localhost:6000/api/";
+export const base_img = "http://localhost:6000/uploads/";
